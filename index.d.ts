a804211d58168e9909cdc7b4382b0716cc4bd4e9
EGOIST <0x142857@gmail.com>
2020-12-26 14:57:11 +0800

    feat: update bots and add types

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+const isBot: (userAgent: string) => boolean;
+
+export { isBot };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const bots = require('./bots')
+const bots = require("./bots");
 
-module.exports = ua => bots.some(bot => {
-  const re = new RegExp(bot)
-  return re.test(ua)
-})
+exports.isBot = (ua) =>
+  bots.some((bot) => {
+    const re = new RegExp(bot);
+    return re.test(ua);
+  });
